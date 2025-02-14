a0a91d7d32459f5b6ec280f42a2562d4a20ad58c
Archit Singh <118463827+architsinghhh@users.noreply.github.com>
2025-02-14 13:38:34 +0530

    Update script.js

--- Javascript&JSON/script.js
+++ Javascript&JSON/script.js
@@ -55,7 +55,7 @@ loginBtn.addEventListener('click', async function () {
   }
 
   try {
-      const response = await fetch('http://127.0.0.1:5000/login', {
+      const response = await fetch('https://deployement-2-zi6r.onrender.com', {
           method: 'POST',
           headers: {
               'Content-Type': 'application/json',
